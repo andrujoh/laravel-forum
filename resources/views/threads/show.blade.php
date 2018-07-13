45d48aefc980ad45db3e859ed5a0fb592d77a15d
andrujoh <andru_joh@yahoo.no>
2018-07-14 00:51:47 +0200

    Added replies to threads

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -2,19 +2,36 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{$thread->title}}</div>
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-header">{{$thread->title}}</div>
 
-                <div class="card-body">
-                  <article>
-                    <p>{{$thread->body}}</p>
-                  </article>
-                  <hr>
-                </div>
-            </div>
+        <div class="card-body">
+          <article>
+            <p>{{$thread->body}}</p>
+          </article>
+          <hr>
         </div>
+      </div>
     </div>
+  </div>
+
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      {{-- {{dd($thread->replies)}} --}}
+      @foreach ($thread->replies as $reply)
+        <div class="card">
+          <div class="card-header">
+            {{$reply->owner->name}} said {{$reply->created_at->diffForHumans()}}
+          </div>
+          <div class="card-body">
+            {{$reply->body}}
+            <hr>
+          </div>
+        </div>
+      @endforeach
+    </div>
+  </div>
 </div>
 @endsection
---
+++ tests/Unit/ReplyTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ReplyTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function it_has_an_owner()
+    {
+        $reply = factory('App\Reply')->create();
+
+        $this->assertInstanceOf('App\User', $reply->owner);
+    }
+}
